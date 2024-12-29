af160a60b75b5a762a58825c9cc4d42b5734f92e
Jan Str <janstr06@gmail.com>
2024-12-30 00:24:32 +0100

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,15 @@
 // src/components/Navbar.jsx
 import React from "react";
-
+import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
     <nav>
-      <a href="/">Home</a> | <a href="/register">Register</a> |{" "}
-      <a href="/challenges">Challenges</a> | <a href="/instances">Instances</a>
+      <Link to="/">Home</Link> | <Link to="/register">Register</Link> |{" "} 
+      <Link to="/challenges">Challenges</Link> | <Link to="/instances">Instances</Link>
+    {
+      //<a href="/">Home</a> | <a href="/register">Register</a> |{" "}
+      //<a href="/challenges">Challenges</a> | <a href="/instances">Instances</a>
+    }
     </nav>
   );
 }
